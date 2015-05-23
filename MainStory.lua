ac5f29f31b616b29cb5568bca7234f86e3cefe53
WhosMyName <chikangwu95@gmail.com>
2015-05-23 18:50:22 +0200

    MS
    Pathway, Demon-Forrest and Fighting!

--- MainStory.lua
+++ MainStory.lua
@@ -4,7 +4,7 @@ intro00001 = [[
 Katheryn Alyayna awoke.
 It was so darn dark in the Room, in which she awoke, she could barely see her own hands. 
 Her back was wet from the soaked Hay she arose from.
-She ignited a Spark to light up the Room, centering itself in the middle of the temporary imprisonment.
+She conjured a Spark to light up the Room, centering itself in the middle her confinement.
 In the darkest Corner, she found the Outline of what could be a Chest.
 On the opposite side of the Room, a Door symbolized in runic Letters.]],
 
@@ -13,7 +13,7 @@ interactchest0001 = [[
 She moved towards the Chest.
 With the small Amount of Strength she had left, she tried to open the Chest.
 She threw her whole Weight against the Lock and luckily the Chest Lid flew open, only to back-swing and injured Katheryn.
-Blood dribbled down her Ribs. --Bleeding
+Blood dribbled down her Ribs, but it was only a minor cut, barely to be noticeable.
 Panting.
 The Odds where with her, as she found Cloth Rags, a Sack and a Robe.]],
 -- Inventory add: Cloth Rags, Sack, Robe
@@ -25,19 +25,39 @@ As she touched the Letters, her Spark faded in the blink of a second.
 It was dark again.
 But then a sudden reddish glow emerged from the ensign, a crackle to be heard.
 Flames burst forth, enlightening the housing.
-In panic she stumbled backward.
+Frightened she stumbled backward.
 The Door manifested and swung open, whilst the Flames started to devour the Building.
 Realizing the new hazard was about to consume her life, she made a run, startled by her fear.
 Emerging from the Ruins of her former Imprisonment, Katheryn, weak and broken, took a look at the new Environment, while enjoying her freedom with a sigh of release.]],
 
 -- Area: Forrest Island
 intro00002 = [[
+Katheryn moved forward, only to be surprised by a Lakeview.
 The Lake, clear, smooth and of a Color dark as the Night surrounding her, lay still.
 Awaiting.
 She could see the mirror image of the house burning apart, while feeling the heat in her neck.
 The Moon, full and bright climbed the sky-plane, putting the Scene in a gloomy Shade.
-She turned around to see Trees. Lots of Trees.
+She turned to find herself trapped in an unknown Forrest.
 Lost in a Forrest at Night with the only possible Shelter burning down to a skeleton.
-But there it was, a pathway leading through the Forrest.
+But there it was, a Pathway leading through the Forrest.
 ]],
+
+-- Interact: Pathway
+interactway0001 = [[
+Although the Pathway was her only possibility to escape, Katheryn shuddered at the thought of venturing into the dark Woods.
+A last look at the remainders of her new memories, flickering in the light air breeze, Sparks blurring her vision.
+She turned around and invoked the ember Flame, a small Familiar to grant Light if needed.
+Advancing, while trying to fight the rampant Foliage, Katheryn heard a loud scrambling noise.
+It seemed to get closer to her, but she couldn't determine the Position of the Sound.
+Distorted by the Forrest, thrown back by the Trees, it was impossible for Katheryn to focus on the Source of the Sound.
+Shadows where none were and a creeping Sound surrounding her, cause a Panic which stirred up and overtook her thoughts.
+Katheryn started to run, following the Pathway in hope of a safe exit.
+But there was no escape from the demonic Foliage around her.
+She tripped and fell over, rolling down an inclined slope.
+Her Body was stopped by brute Force, crashing against a Trunk.
+Losing consciousness and vision, the imminent Shadow approaching her.
+A Slash and a Flash of Pain recalling her Senses, as her primal Instincts kicked in.
+Crippled, but willing, she stood up to fight her Opponent.
+]],
+
 }
